db776027090c39da426339b2a9e7a39262c21e87
Kateryna <akvilegia83@gmail.com>
2026-05-29 14:16:11 +0300

    remove string 18

--- app/main.py
+++ app/main.py
@@ -15,7 +15,6 @@ students = ["John", "Bob", "Alice", "Anna"]  # list
 fruits = {"apple", "banana", "grape"}  # set
 user = {"name": "John", "age": 25}  # dict
 # %%
-
 sorted_variables = {
     "mutable": [profile_info, collection_of_coins, students, fruits, user],
     "immutable": [count_people, is_empty, marks, message, price,]
