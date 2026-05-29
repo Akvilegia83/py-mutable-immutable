4835a0b8437b265986229f3de51f1478d1430e03
Kateryna <akvilegia83@gmail.com>
2026-05-29 11:54:00 +0300

    stile correction

--- app/main.py
+++ app/main.py
@@ -7,7 +7,7 @@ message = "Hello, world!"  # str
 # %%
 # colections
 marks = (2, 3, 4, 5)  # tuple
-
+# %%
 # mutable
 profile_info = {"name": "Test"}
 collection_of_coins = [1, 2, 3]
@@ -16,7 +16,5 @@ fruits = {"apple", "banana", "grape"}  # set
 user = {"name": "John", "age": 25}  # dict
 # %%
 
-sorted_variables = {
-    "mutable":[profile_info, collection_of_coins, students, fruits, user,], 
-    "immutable":[count_people, is_empty, marks, message, price,]
-}
+sorted_variables = {"mutable": [profile_info, collection_of_coins, students, fruits, user,], "immutable": [count_people, is_empty, marks, message, price,]}
+
