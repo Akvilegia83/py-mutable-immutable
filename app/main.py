c438c4701ba2ec563dfb2a8a04fc827697e06931
Kateryna <akvilegia83@gmail.com>
2026-05-31 16:35:14 +0300

    added string 18

--- app/main.py
+++ app/main.py
@@ -5,8 +5,9 @@ is_empty = True  # bool
 message = "Hello, world!"  # str
 
 # %%
-# colections
+# collections
 marks = (2, 3, 4, 5)  # tuple
+
 # %%
 # mutable
 profile_info = {"name": "Test"}
@@ -14,8 +15,10 @@ collection_of_coins = [1, 2, 3]
 students = ["John", "Bob", "Alice", "Anna"]  # list
 fruits = {"apple", "banana", "grape"}  # set
 user = {"name": "John", "age": 25}  # dict
+
 # %%
 sorted_variables = {
     "mutable": [profile_info, collection_of_coins, students, fruits, user],
     "immutable": [count_people, is_empty, marks, message, price,]
 }
+
