ab586f2ea33cd7f8ab05cf1ae93bba92dea4ce92
Kateryna <akvilegia83@gmail.com>
2026-06-01 20:11:29 +0300

    remove profileinfo and coins from sorted variables

--- app/main.py
+++ app/main.py
@@ -16,6 +16,6 @@ user = {"name": "John", "age": 25}  # dict
 
 # %%
 sorted_variables = {
-    "mutable": [profile_info, collection_of_coins, students, fruits, user],
+    "mutable": [students, fruits, user],
     "immutable": [count_people, marks, is_empty, message, price]
 }
