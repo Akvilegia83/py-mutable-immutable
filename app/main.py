b6971280efe896afe6859dc63bef165691890b75
Kateryna <akvilegia83@gmail.com>
2026-06-02 12:09:29 +0300

    Deleted all code

--- app/main.py
+++ app/main.py
@@ -1,21 +1,2 @@
-# %%
-count_people = 5  # int
-price = 13.25  # float
-is_empty = True  # bool
-message = "Hello, world!"  # str
 
-# %%
-# collections
-marks = (2, 3, 4, 5)  # tuple
 
-# %%
-# mutable
-students = ["John", "Bob", "Alice", "Anna"]  # list
-fruits = {"apple", "banana", "grape"}  # set
-user = {"name": "John", "age": 25}  # dict
-
-# %%
-sorted_variables = {
-    "mutable": [students, fruits, user],
-    "immutable": [count_people, marks, is_empty, message, price]
-}
