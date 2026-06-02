cee154d0061b61e5eae93e9fa316ce2dbe649cf1
Kateryna <akvilegia83@gmail.com>
2026-06-02 12:42:37 +0300

    created new code

--- app/main.py
+++ app/main.py
@@ -1,2 +1,22 @@
+lucky_number = 777  # int
+pi = 3.14  # float
+one_is_a_prime_number = False  # bool
+name = "Richard"  # str
+my_favorite_films = [
+    "The Shawshank Redempion",
+    "The Lord of the Rings: The Return of the King",
+    "Pulp Fiction",
+    "The Good, the Bad and the Ugly",
+    "The Matrix",
+]  # list
+profile_info = ("michel", "michel@gmail.com", "12345678")  # tuple
+marks = {
+    "John": 4,
+    "Sergio": 3,
+}  # dict
+collection_of_coins = {1, 2, 25}  # set
 
-
+sorted_variables = {
+    "mutable": [my_favorite_films, marks, collection_of_coins],
+    "immutable": [lucky_number, pi, one_is_a_prime_number, name, profile_info,]
+}
