b689c7dc94a39e03d85ed7abe56ae5ac4dea3ab9
Kateryna <akvilegia83@gmail.com>
2026-06-02 12:57:47 +0300

    deleted empty string

--- app/main.py
+++ app/main.py
@@ -15,7 +15,6 @@ marks = {
     "Sergio": 3,
 }  # dict
 collection_of_coins = {1, 2, 25}  # set
-
 sorted_variables = {
     "mutable": [my_favorite_films, marks, collection_of_coins],
     "immutable": [lucky_number, pi, one_is_a_prime_number, name, profile_info,]
